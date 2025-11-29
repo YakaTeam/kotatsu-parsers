957d65d6bf918a4b5aa4fc1c5c8da70525b41f9d
dragonx943 <premieregirl26@gmail.com>
2025-11-30 01:16:41 +0700

    chore(Manhuauss): Update source domain (close #10)

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/Manhuauss.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/Manhuauss.kt
@@ -7,6 +7,6 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("MANHUAUSS", "Manhuauss", "en")
 internal class Manhuauss(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.MANHUAUSS, "manhuauss.com") {
+	MadaraParser(context, MangaParserSource.MANHUAUSS, "manhuaus.com") {
 	override val withoutAjax = true
 }
