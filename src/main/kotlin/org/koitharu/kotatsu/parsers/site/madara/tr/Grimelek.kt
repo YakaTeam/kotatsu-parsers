97a0bffe9cc78d7c9703f1f3779881aa7d92ada7
Draken <131387159+dragonx943@users.noreply.github.com>
2025-12-20 23:08:01 +0700

    Fix build

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Grimelek.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/tr/Grimelek.kt
@@ -1,11 +1,10 @@
 package org.koitharu.kotatsu.parsers.site.madara.tr
 
-import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import org.koitharu.kotatsu.parsers.exception.AuthRequiredException
+import org.koitharu.kotatsu.parsers.util.getCookies
 
 //This source requires an account.
 @MangaSourceParser("GRIMELEK", "SiyahMelek", "tr")
@@ -14,11 +13,11 @@ internal class Grimelek(context: MangaLoaderContext) :
 	override val datePattern = "d MMMM yyyy"
 	override val listUrl = "seri/"
 	override val authUrl: String
-        get() = "https://${domain}"
+		get() = "https://${domain}"
 
-    override suspend fun isAuthorized(): Boolean {
-        return context.cookieJar.getCookies(domain).any {
-            it.name.contains("wordpress_logged")
-        }
+	override suspend fun isAuthorized(): Boolean {
+		return context.cookieJar.getCookies(domain).any {
+			it.name.contains("wordpress_logged")
+		}
 	}
 }
