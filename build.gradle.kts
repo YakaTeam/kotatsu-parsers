81a58435503989e60d6ab0c0138e4c210ba53edd
dragonx943 <premieregirl26@gmail.com>
2026-03-17 20:34:06 +0700

    chore(workflows): Add new workflows for triggering and publishing packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,77 @@
 import tasks.ReportGenerateTask
 
 plugins {
-    `java-library`
-    `maven-publish`
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ksp)
+	`java-library`
+	`maven-publish`
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.ksp)
 }
 
 group = "org.koitharu"
-version = "1.0"
+version = System.getenv("RELEASE_VERSION") ?: "1.2.3-SNAPSHOT"
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 ksp {
-    arg("summaryOutputDir", "${projectDir}/.github")
+	arg("summaryOutputDir", "${projectDir}/.github")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    compilerOptions {
-        freeCompilerArgs.addAll(
-            "-opt-in=kotlin.RequiresOptIn",
-            "-opt-in=kotlin.contracts.ExperimentalContracts",
-            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-opt-in=org.koitharu.kotatsu.parsers.InternalParsersApi",
-        )
-    }
+	compilerOptions {
+		freeCompilerArgs.addAll(
+			"-opt-in=kotlin.RequiresOptIn",
+			"-opt-in=kotlin.contracts.ExperimentalContracts",
+			"-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+			"-opt-in=org.koitharu.kotatsu.parsers.InternalParsersApi",
+		)
+	}
 }
 
 kotlin {
-    jvmToolchain(11)
-    explicitApiWarning()
-    sourceSets["main"].kotlin.srcDirs("build/generated/ksp/main/kotlin")
+	jvmToolchain(11)
+	explicitApiWarning()
+	sourceSets["main"].kotlin.srcDirs("build/generated/ksp/main/kotlin")
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-    }
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+			groupId = "org.koitharu"
+			artifactId = "kotatsu-parsers"
+			version = project.version.toString()
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY") ?: "KotatsuApp/kotatsu-parsers"}")
+			credentials {
+				username = System.getenv("GITHUB_ACTOR")
+				password = System.getenv("GITHUB_TOKEN")
+			}
+		}
+	}
 }
 
 dependencies {
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.okhttp)
-    implementation(libs.okio)
-    implementation(libs.json)
-    implementation(libs.androidx.collection)
-    api(libs.jsoup)
+	implementation(libs.kotlinx.coroutines.core)
+	implementation(libs.okhttp)
+	implementation(libs.okio)
+	implementation(libs.json)
+	implementation(libs.androidx.collection)
+	api(libs.jsoup)
 
-    ksp(project(":kotatsu-parsers-ksp"))
+	ksp(project(":kotatsu-parsers-ksp"))
 
-    testImplementation(libs.junit.api)
-    testImplementation(libs.junit.engine)
-    testImplementation(libs.junit.params)
-    testRuntimeOnly(libs.junit.launcher)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.quickjs)
+	testImplementation(libs.junit.api)
+	testImplementation(libs.junit.engine)
+	testImplementation(libs.junit.params)
+	testRuntimeOnly(libs.junit.launcher)
+	testImplementation(libs.kotlinx.coroutines.test)
+	testImplementation(libs.quickjs)
 }
 
 tasks.register<ReportGenerateTask>("generateTestsReport")
